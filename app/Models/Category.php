dcc8447b393143639fa3c2b7e82b2451bf9607b7
Mena Ehab <127118141+menaehab@users.noreply.github.com>
2025-06-29 23:34:02 +0300

    feat: add category (CRUD)

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasSlug;
+
+    protected $guarded = ['id'];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+}
